e0a24dbe54446f2c555cb58ef0b1412084c8cfe9
woonhk90 <wonho7344@gmail.com>
2022-08-18 14:52:13 +0900

    [fix] 타임아웃테스트

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -27,14 +27,14 @@ const Login = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [login, setLogin] = React.useState({
-    userId: '',
+    username: '',
     password: '',
   });
 
-  const {userId,password}=login
+  const {username,password}=login
 
   const onSubmitEventHandler = async () => {
-    if(userId.trim()===''){
+    if(username.trim()===''){
       window.alert("아이디를 입력해주세요.");
       return false;
     }
@@ -77,7 +77,7 @@ const Login = () => {
           <LoginTitle>로그인</LoginTitle>
           <LoginForm>
             <div>
-              <Input type={'text'} width={'500px'} id={'userId'} name={'userId'} maxLength={'20'} onChange={onChangeEventHandler} placeholder={"아이디를 입력하세요."} autoFocus={'autoFocus'} />
+              <Input type={'text'} width={'500px'} id={'username'} name={'username'} maxLength={'20'} onChange={onChangeEventHandler} placeholder={"아이디를 입력하세요."} autoFocus={'autoFocus'} />
             </div>
             <div>
               <Input type={'password'} width={'500px'} id={'password'} name={'password'} maxLength={'20'} onChange={onChangeEventHandler} placeholder={"비밀번호를 입력하세요."} />
