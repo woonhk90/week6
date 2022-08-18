7194fafc51e547e76eb4f6610d86c698f438b9fd
DESKTOP-KBMLAIH\jsi98 <sij7304@gmail.com>
2022-08-18 15:37:46 +0900

    css 작업중

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,10 +4,10 @@ import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { __postLogin } from "../redux/modules/todoSlice";
-import Button from './elements/Button';
-import Input from './elements/Input';
+import Button from "./elements/Button";
+import Input from "./elements/Input";
 
-import bgImg from '../img/bg_img.jpg';
+import bgImg from "../img/programming.jpg";
 
 import axios from "axios";
 import Cookies from "universal-cookie";
@@ -21,54 +21,54 @@ export function setRefreshTokenToCookie(data) {
   cookies.set("Authorization", data, { path: "/", expires: now });
 }
 
-
-
 const Login = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [login, setLogin] = React.useState({
-    userId: '',
-    password: '',
+    userId: "",
+    password: "",
   });
 
-  const {userId,password}=login
+  const { userId, password } = login;
 
   const onSubmitEventHandler = async () => {
-    if(userId.trim()===''){
+    if (userId.trim() === "") {
       window.alert("아이디를 입력해주세요.");
       return false;
     }
-    if(password.trim()===''){
+    if (password.trim() === "") {
       window.alert("비밀번호를 입력해주세요.");
       return false;
     }
     try {
-      const data = await axios.post(`${process.env.REACT_APP_IP_ADDRESS}/member/login`, login, {
-        // const data = await axios.post(`${process.env.REACT_APP_TEST_IP_ADDRESS}/login`, login, {
-        headers: {
-
-        },
-      });
+      const data = await axios.post(
+        `${process.env.REACT_APP_IP_ADDRESS}/member/login`,
+        login,
+        {
+          // const data = await axios.post(`${process.env.REACT_APP_TEST_IP_ADDRESS}/login`, login, {
+          headers: {},
+        }
+      );
       console.log("로그인성공데이터1:", data);
       console.log("로그인성공데이터2:", data.status);
       /* 트루이면 */
       // data?navigate('/main'):window.alert("로그인 실패하였습니다.");
       const token = data.headers.authorization;
       setRefreshTokenToCookie(token);
-      data.status === 200 ? navigate('/main') : window.alert("로그인 실패하였습니다.");
+      data.status === 200
+        ? navigate("/main")
+        : window.alert("로그인 실패하였습니다.");
     } catch {
-      alert('사용자를 찾을 수 없습니다.');
+      alert("사용자를 찾을 수 없습니다.");
     }
-
-
-  }
+  };
   const onChangeEventHandler = (e) => {
     const { name, value } = e.target;
     setLogin({
       ...login,
-      [name]: value
-    })
-  }
+      [name]: value,
+    });
+  };
 
   return (
     <>
@@ -77,49 +77,71 @@ const Login = () => {
           <LoginTitle>로그인</LoginTitle>
           <LoginForm>
             <div>
-              <Input type={'text'} width={'500px'} id={'userId'} name={'userId'} maxLength={'20'} onChange={onChangeEventHandler} placeholder={"아이디를 입력하세요."} autoFocus={'autoFocus'} />
+              <Input
+                type={"text"}
+                width={"500px"}
+                id={"userId"}
+                name={"userId"}
+                maxLength={"20"}
+                onChange={onChangeEventHandler}
+                placeholder={"아이디를 입력하세요."}
+                autoFocus={"autoFocus"}
+              />
             </div>
             <div>
-              <Input type={'password'} width={'500px'} id={'password'} name={'password'} maxLength={'20'} onChange={onChangeEventHandler} placeholder={"비밀번호를 입력하세요."} />
+              <Input
+                type={"password"}
+                width={"500px"}
+                id={"password"}
+                name={"password"}
+                maxLength={"20"}
+                onChange={onChangeEventHandler}
+                placeholder={"비밀번호를 입력하세요."}
+              />
             </div>
-            <Button btntype="login" onClick={() => (onSubmitEventHandler())}>로그인</Button>
-            <Button btntype="signup" onClick={() => (navigate('/signup'))}>회원가입</Button>
+            <Button btntype='login' onClick={() => onSubmitEventHandler()}>
+              로그인
+            </Button>
+            <Button btntype='signup' onClick={() => navigate("/signup")}>
+              회원가입
+            </Button>
           </LoginForm>
         </LoginWrap>
       </ImgDiv>
     </>
-  )
-}
+  );
+};
 
 export default Login;
 
 const ImgDiv = styled.div`
-  width:100vw;
-  height:100vh;
-  overflow:hidden;
-  background:url(${bgImg}) no-repeat 50% 50%;background-size:cover;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  background: url(${bgImg}) no-repeat 50% 50%;
+  background-size: cover;
 
-  display:flex;
-  justify-content:center;
-  align-items:center;
-  color:#fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
 `;
 
 const LoginWrap = styled.div`
-    width:1000px;
-    background-color:rgba(0,0,0,0.4);
-    text-align:center;
-    display:flex;
-    flex-direction:column;
+  width: 1000px;
+  background-color: rgba(0, 0, 0, 0.8);
+  text-align: center;
+  display: flex;
+  flex-direction: column;
 `;
 const LoginTitle = styled.h1`
-    line-height:80px;
-    font:40px/80px 'Arial','sans-serif';
-    letter-spacing: 10px;
+  line-height: 80px;
+  font: 40px/80px "Arial", "sans-serif";
+  letter-spacing: 10px;
 `;
 const LoginForm = styled.div`
-    font:20px/80px 'Arial','sans-serif';
-    letter-spacing: 5px;
-    padding:15px;
-    box-sizing:border-box;
+  font: 20px/80px "Arial", "sans-serif";
+  letter-spacing: 5px;
+  padding: 15px;
+  box-sizing: border-box;
 `;
